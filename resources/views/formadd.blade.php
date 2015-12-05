3f86a251a4247f9a2608347331167c779f653bf4
shufisyahida <semangkaforever@gmail.com>
2015-12-05 20:53:53 +0700

    form add

--- resources/views/formadd.blade.php
+++ resources/views/formadd.blade.php
@@ -5,57 +5,51 @@
 @section('header')
     <div class="header">
         <div class="container" id="headerContainer">
-            <div class="row">
-                <button type="button" class="btn btn-link"><h4>ATMoo</h4></button>
-            </div>
+            <span class="logo"><a href="{{url('/')}}">ATMoo</a></span>
         </div>
     </div>
 
 @endsection
 
 @section('content')
-    <div class="form">
-        <div class="container" id="headerContainer"></div>
-            <h4 class="text-center">Please Fill The Form</h4>
-        </div>      
-    </div>
-
-    <form class="form-horizontal">
-        <div class="form-group">
-            <label for="inputName" class="col-sm-2 control-label">ATM's Name</label>
-            <div class="col-xs-4">
-                <input type="Name" class="form-control" id="inputName" placeholder="Write the Name of ATM">
-            </div>
-        </div>
-  
-        <div class="form-group">
-            <label for="inputLocation" class="col-sm-2 control-label">ATM's Location</label>
-            <div class="col-xs-4">
-                <input type="Location" class="form-control" id="inputLocation" placeholder="Write the Location of ATM">
-            </div>
-        </div>
-  
-        <div class="form-group">
-            <label for="inputFiture" class="col-sm-2 control-label">ATM's Fiture</label>
-        </div>
-        
-        <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-                <div class="checkbox">
-                    <label class="checkbox-inline">
-                        <input type="checkbox"> Setor Tunai
-                    </label>
-                    <label class="checkbox-inline">
-                        <input type="checkbox"> Tarik Tunai
-                    </label>
+    <div class="container map">
+        <div class="card">
+            <h3 class="text-center">Please Fill The Form</h3><br>
+            <div class="container-form">
+            <form class="form-horizontal">
+                <div class="form-group">
+                    <label for="inputName" class="col-sm-2 control-label">ATM's Name</label>
+                    <div class="col-sm-10">
+                        <input type="Name" class="form-control" id="inputName" placeholder="Write the Name of ATM">
+                    </div>
                 </div>
+          
+                <div class="form-group">
+                    <label for="inputLocation" class="col-sm-2 control-label">ATM's Location</label>
+                    <div class="col-sm-10">
+                        <input type="Location" class="form-control col-md-8" id="inputLocation" placeholder="Write the Location of ATM">
+                    </div>
+                </div>
+          
+                <div class="form-group">
+                    <label for="inputFiture" class="col-sm-2 control-label">ATM's Fiture</label>
+                    <div class="checkbox col-sm-10">
+                        <label>
+                            <input type="checkbox"> Setor Tunai
+                        </label>
+                        <label>
+                            <input type="checkbox"> Tarik Tunai
+                        </label>
+                    </div>
+                </div>
+
+                <div class="form-group" style="padding-top: 10px">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" class="btn btn-pink">Add ATM</button>
+                    </div>
+                </div>
+            </form>
             </div>
         </div>
-  
-        <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-danger">Add ATM</button>
-            </div>
-        </div>
-    </form>
+    </div>
 @endsection
