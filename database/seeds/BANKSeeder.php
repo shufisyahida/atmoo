e2dcd45922c8cd132e114df4db7b9d4e29f62297
Indah Ayu Fauziah <indahayu24@gmail.com>
2015-12-06 01:32:34 +0200

    database

---
+++ database/seeds/BANKSeeder.php
@@ -0,0 +1,98 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BANKSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bank')->insert(['nama' => 'Bank Mandiri']);
+        DB::table('bank')->insert(['nama' => 'BRI']);
+        DB::table('bank')->insert(['nama' => 'BNI']);
+        DB::table('bank')->insert(['nama' => 'Bank CIMB Niaga']);
+        DB::table('bank')->insert(['nama' => 'PaninBank']);
+        DB::table('bank')->insert(['nama' => 'PermataBank']);
+        DB::table('bank')->insert(['nama' => 'Bank Danamon']);
+        DB::table('bank')->insert(['nama' => 'BII']);
+        DB::table('bank')->insert(['nama' => 'Bank BTN']);
+        DB::table('bank')->insert(['nama' => 'Bank NISP']);
+        DB::table('bank')->insert(['nama' => 'Bank JABAR']);
+        DB::table('bank')->insert(['nama' => 'Bank Mega']);
+        DB::table('bank')->insert(['nama' => 'Bank Bukopin']);
+        DB::table('bank')->insert(['nama' => 'Bank HSBC']);
+        DB::table('bank')->insert(['nama' => 'CITIBANK']);
+        DB::table('bank')->insert(['nama' => 'Bank UOB']);
+        DB::table('bank')->insert(['nama' => 'Bank BTPN']);
+        DB::table('bank')->insert(['nama' => 'Bank Syariah Mandiri']);
+        DB::table('bank')->insert(['nama' => 'Standard Chartered Bank']);
+        DB::table('bank')->insert(['nama' => 'Bank Muamalat']);
+        DB::table('bank')->insert(['nama' => 'Bank DBS']);
+        DB::table('bank')->insert(['nama' => 'Bank Pembangunan Daerah KALTIM']);
+        DB::table('bank')->insert(['nama' => 'Bank JATIM']);
+        DB::table('bank')->insert(['nama' => 'Bank ANZ']);
+        DB::table('bank')->insert(['nama' => 'Bank DKI']);
+        DB::table('bank')->insert(['nama' => 'Bank JATENG']);
+        DB::table('bank')->insert(['nama' => 'Bank Ekonomi']);
+        DB::table('bank')->insert(['nama' => 'Bank Pembangunan Daerah SUMUT']);
+        DB::table('bank')->insert(['nama' => 'Bank RIAU']);
+        DB::table('bank')->insert(['nama' => 'Bank Mayapada']);
+        DB::table('bank')->insert(['nama' => 'Bank SUMSEL']);
+        DB::table('bank')->insert(['nama' => 'Bank Mutiara']);
+        DB::table('bank')->insert(['nama' => 'Bank Sinarmas']);
+        DB::table('bank')->insert(['nama' => 'Bank Pembangunan Daerah PAPUA']);
+        DB::table('bank')->insert(['nama' => 'Commonwealth Bank']);
+        DB::table('bank')->insert(['nama' => 'Bank Nagari']);
+        DB::table('bank')->insert(['nama' => 'Bank RIS']);
+        DB::table('bank')->insert(['nama' => 'Bank Rabobank']);
+        DB::table('bank')->insert(['nama' => 'Bank of China']);
+        DB::table('bank')->insert(['nama' => 'Bank Pembangunan Daerah ACEH']);
+        DB::table('bank')->insert(['nama' => 'Bank Pembangunan Daerah BALI']);
+        DB::table('bank')->insert(['nama' => 'Bank KALSEL']);
+        DB::table('bank')->insert(['nama' => 'Bank KALBAR']);
+        DB::table('bank')->insert(['nama' => 'Bank BNP']);
+        DB::table('bank')->insert(['nama' => 'Bank Mega Syariah']);
+        DB::table('bank')->insert(['nama' => 'Bank PUNDI']);
+        DB::table('bank')->insert(['nama' => 'Bank Saudara']);
+        DB::table('bank')->insert(['nama' => 'Bank SULSEL']);
+        DB::table('bank')->insert(['nama' => 'Bank ICBC']);
+        DB::table('bank')->insert(['nama' => 'Bank Mestika']);
+        DB::table('bank')->insert(['nama' => 'Bank NTT']);
+        DB::table('bank')->insert(['nama' => 'Bank SULUT']);
+        DB::table('bank')->insert(['nama' => 'Bank CAPITAL']);
+        DB::table('bank')->insert(['nama' => 'Bank Pembangunan Daerah DIY']);
+        DB::table('bank')->insert(['nama' => 'Bank Lampung']);
+        DB::table('bank')->insert(['nama' => 'Bank QNB']);
+        DB::table('bank')->insert(['nama' => 'Bank Maluku']);
+        DB::table('bank')->insert(['nama' => 'Bank BJB']);
+        DB::table('bank')->insert(['nama' => 'Bank Index']);
+        DB::table('bank')->insert(['nama' => 'Bank NTB']);
+        DB::table('bank')->insert(['nama' => 'Bank Agro']);
+        DB::table('bank')->insert(['nama' => 'Bank KALTENG']);
+        DB::table('bank')->insert(['nama' => 'Bank JAMBI']);
+        DB::table('bank')->insert(['nama' => 'Bank Kesejerahteraan']);
+        DB::table('bank')->insert(['nama' => 'Bank SULTRA']);
+        DB::table('bank')->insert(['nama' => 'Bank BENGKULU']);
+        DB::table('bank')->insert(['nama' => 'Bank Swadesi']);
+        DB::table('bank')->insert(['nama' => 'Bank Mayora']);
+        DB::table('bank')->insert(['nama' => 'Bank Ganesha']);
+        DB::table('bank')->insert(['nama' => 'Bank INA']);
+        DB::table('bank')->insert(['nama' => 'Bank SULTENG']);
+        DB::table('bank')->insert(['nama' => 'Nobubank']);
+        DB::table('bank')->insert(['nama' => 'PaninBank Syariah']);
+        DB::table('bank')->insert(['nama' => 'Bank ARTOS']);
+        DB::table('bank')->insert(['nama' => 'Bank EKA']);
+        DB::table('bank')->insert(['nama' => 'BPR KS']);
+        DB::table('bank')->insert(['nama' => 'Bank ANDARA']);
+        DB::table('bank')->insert(['nama' => 'Bank BUMIPUTERA']);
+        DB::table('bank')->insert(['nama' => 'Bank Woori Indonesia']);
+        DB::table('bank')->insert(['nama' => 'BPR Bank Supra']);
+        DB::table('bank')->insert(['nama' => 'BCA']);
+        DB::table('bank')->insert(['nama' => 'ATM Bersama']);
+
+    }
+}
